e6e3a6270265f925ee85fd56fa36e19b9f1cb4e7
Alexdevsoft <alexhavilla2022@gmail.com>
2023-03-02 16:13:02 -0300

    DepartmentList controller

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -14,6 +14,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
