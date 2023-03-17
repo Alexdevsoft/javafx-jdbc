4ed01ff1370ee0c75dc60ed693e78dc02b087d66
Alexdevsoft <alexhavilla2022@gmail.com>
2023-03-16 23:11:06 -0300

    Validation Exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
@@ -70,6 +73,9 @@ public class DepartmentFormController implements Initializable{
 			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
 		}
+		catch(ValidationException e) {
+			setErrorMessages(e.getErros());
+		}
 		catch(DbException e){
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
@@ -84,8 +90,14 @@ public class DepartmentFormController implements Initializable{
 
 	private Department getFormData() {
 		Department obj = new Department();
-		
+		ValidationException exception = new ValidationException("Validation error");
 		obj.setId(Utils.tryParseToInt(textId.getText()));
+		if(textName.getText() == null || textName.getText().trim().equals("")) {
+			exception.addError("name", "Field can't be empty");
+		}
+		if(exception.getErros().size() > 0) {
+			throw exception;
+		}
 		obj.setName(textName.getText());
 		
 		return obj;
@@ -113,6 +125,14 @@ public class DepartmentFormController implements Initializable{
 		}
 		textId.setText(String.valueOf(entity.getId()));
 		textName.setText(entity.getName());
+	}
+	
+	public void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("name")) {
+			lableErrorName.setText(errors.get("name"));
+		}
 	}
 
 }
